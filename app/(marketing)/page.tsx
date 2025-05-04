779ffc287d83827134cf9e3a7de626b849e8ffe1
Jerry Ding <jerryding123@gmail.com>
2025-05-05 00:44:49 +0800

    v3

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -65,7 +65,7 @@ import faq from '#data/faq'
 import pricing from '#data/pricing'
 import testimonials from '#data/testimonials'
 
-export const meta: Metadata = {
+export const metadata: Metadata = {
   title: 'Saas UI Landingspage',
   description: 'Free SaaS landingspage starter kit',
 }
