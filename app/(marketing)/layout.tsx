194d7c50bd80787a65cb20f22b48b531fb6d491e
Jerry Ding <jerryding123@gmail.com>
2025-06-16 19:40:59 +0800

    Added Vercel Web Analytics

--- app/(marketing)/layout.tsx
+++ app/(marketing)/layout.tsx
@@ -1,5 +1,11 @@
 import { MarketingLayout } from '#components/layout'
+import { Analytics } from '@vercel/analytics/react'
 
 export default function Layout(props: { children: React.ReactNode }) {
-  return <MarketingLayout>{props.children}</MarketingLayout>
+  return (
+    <MarketingLayout>
+      {props.children}
+      <Analytics />
+    </MarketingLayout>
+  )
 }
